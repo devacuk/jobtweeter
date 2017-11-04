2a3951e41cae3ba79c06e95ed3d408be53ac6f17
martin-hamilton <m@martinh.net>
2017-11-04 10:28:52 +0000

    Improved Slack posts and added long Tweet logic

--- tweet_job.py
+++ tweet_job.py
@@ -36,36 +36,51 @@ driver = webdriver.Chrome(DRIVER)
 driver.get(URL)
 
 # scroll to the bit we are interested in, and take a screenshot
-element = driver.find_element_by_id('layout') # we want the content tagged with "layout"
+element = driver.find_element_by_id('layout')	# we want the content tagged with "layout"
 location = element.location_once_scrolled_into_view
-driver.save_screenshot('screenshot.png') # saves screenshot of entire page
+driver.save_screenshot('screenshot.png')	# saves screenshot of entire page
 driver.quit()
 
 # crop screenshot to just the main content
-im = Image.open('screenshot.png') # uses PIL library to open image in memory
+im = Image.open('screenshot.png')		# uses PIL library to open image in memory
 left = int(location['x'])
 top = int(location['y'])
-right = 650 # hard coded to match jobs.ac.uk content area
-bottom = 670 # hard coded to match jobs.ac.uk content area
-im = im.crop((left, top, right, bottom)) # defines crop points
-im.save('screenshot.png') # saves new cropped image
+right = 650					# hard coded to match jobs.ac.uk content area
+bottom = 670					# hard coded to match jobs.ac.uk content area
+im = im.crop((left, top, right, bottom))	# defines crop points
+im.save('screenshot.png')			# saves new cropped image
 
-# Tweet this job opportunity
-updatetext = "Job opportunity: {0} at {1}, {2} {3} #devacuk via @jobsacuk".format(role, institution, ft, URL)
-print updatetext
-time.sleep(5)
+# Line up a tweet about this job
+tweet_text = "Job opportunity: {0} at {1}, {2} {3} #devacuk via @jobsacuk".format(role, institution, ft, URL)
+if len(tweet_text) > 140:
+  tweet_text = "Job opportunity: {0} at {1}, {2} #devacuk via @jobsacuk".format(role, institution, URL)
+if len(tweet_text) > 140:
+  tweet_text = "Job opportunity: {0} {1} #devacuk via @jobsacuk".format(role, URL)
+print "Tweet: " + tweet_text
 
+# Line up a Slack post about this job
+slack_text = "{0} at {1}, {2} {3}".format(role, institution, ft, URL)
+slack_headline = "{0}".format(role)
+print "Slack headline: " + slack_headline
+print "Slack: " + slack_text
+
+time.sleep(5)					# in case it screws up horribly and we're frantically hitting ^C
+
+# Post it on Twitter
 auth = tweepy.OAuthHandler(cfg.CONSUMER_KEY, cfg.CONSUMER_SECRET)
 auth.set_access_token(cfg.ACCESS_TOKEN, cfg.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-api.update_with_media('screenshot.png', updatetext)
+api.update_with_media('screenshot.png', tweet_text)
 
 # And post it on the #jobs channel on our devacuk Slack instance
 sc = SlackClient(cfg.SLACK_VERIFICATION_TOKEN)
-print sc.api_call(
+sc.api_call(
   'files.upload', 
   channels='jobs', 
-  title=updatetext,
+  title=slack_headline,
   is_public=True,
+  initial_comment=slack_text,
   file=open('screenshot.png', 'rb'),
 )
+
+
